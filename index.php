664465f94874bf5601d5a4d4dd078c06eaa86172
Blas Fernández Segura <burasu@gmail.com>
2015-10-12 23:10:30 +0200

    Creando integración con vistas realizadas en twig

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
 <?php
+
+require 'vendor/autoload.php';
+date_default_timezone_set('Europe/Madrid');
+
+$app = new \Slim\Slim(array(
+	'view' => new \Slim\Views\Twig()		
+));
+
+$view = $app->view();
+$view->parserOptions = array(
+	'debug' => true,
+);
+$view->parserExtensions = array(
+	new \Slim\Views\TwigExtension(),
+);
+
+$app->get('/', function() use($app)
+{
+	$app->render('about.twig');
+});
+
+$app->get('/contact', function() use($app)
+{
+	$app->render('contact.twig');
+});
+
+$app->run();
